7b9676cdfb1e58ae5e6bf7e2f7b4cd9d71df4d40
bogdan_klimov <bodyaklimov333@gmail.com>
2023-12-28 13:04:59 +0200

    load added

--- scripts/pages/blog.js
+++ scripts/pages/blog.js
@@ -1,8 +1,17 @@
-const postsWrapper = document.getElementById('posts')
 const postsCount = 4
+const postsWrapper = document.getElementById('posts')
+const loadList = document.getElementById('load_more-list')
+const leftArrow = document.getElementById('load_more-left')
+const rightArrow = document.getElementById('load_more-right')
+const postCollectionLength = Math.ceil(posts.length / postsCount)
+let extraLoadItems = []
+let loadActiveFlag = true
+let currentPage = 1
+let loadActiveCount = 1
+let loadActiveEnd = 1
 
 const renderPosts = (page) => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0)
     postsWrapper.innerHTML = ''
     for (let i = 0 + postsCount*(page-1); i < postsCount*page; i++) {
         createPost(
@@ -17,12 +26,6 @@ const renderPosts = (page) => {
     }    
 }
 
-const loadList = document.getElementById('load_more-list')
-const leftArrow = document.getElementById('load_more-left')
-const rightArrow = document.getElementById('load_more-right')
-const postCollectionLength = Math.ceil(posts.length / postsCount)
-let currentPage = 1
-
 renderPosts(currentPage)
 
 const createLoadItem = (idx) => {
@@ -33,9 +36,7 @@ const createLoadItem = (idx) => {
 }
 
 const setLoadActive = (idx) => {
-    for (let i = 0; i < loadItems.length; i++) {
-        loadItems[i].classList.remove('active')
-    }
+    for (let i = 0; i < loadItems.length; i++) {loadItems[i].classList.remove('active')}
     loadItems[idx].classList.add('active')
 }
 
@@ -44,15 +45,61 @@ const buttonsControll = () => {
     else leftArrow.classList.remove('disabled')
     if (currentPage == postCollectionLength) rightArrow.classList.add('disabled')
     else rightArrow.classList.remove('disabled')
+}
 
+const renderExtraLoad = () => {
+    extraLoadItems = []
+    for (let i = 0; i < loadItems.length; i++) {
+        if (loadItems[i].innerText == '...') {
+            extraLoadItems.push(loadItems[i])
+        }
+    }
+    for (let i = 0; i < extraLoadItems.length; i++) {
+        extraLoadItems[i].addEventListener('click', () => {
+            const input = document.createElement('input')
+            input.classList.add('load_more-input')
+            extraLoadItems[i].innerHTML = ''
+            extraLoadItems[i].append(input)
+            input.focus()
+            input.addEventListener("focusout", () => {
+                extraLoadItems[i].innerHTML = '...'
+            });
+            input.addEventListener('keydown', (e) => {
+                if (e.key == 'Enter') {
+                    if (input.value > 1 && input.value < postCollectionLength) {
+                        inputValue = Number(input.value)
+                        currentPage = inputValue
+                        initLoad()
+                        if (inputValue == 1) {
+                            setLoadActive(0)
+                        } else if (inputValue == 2) {
+                            setLoadActive(1)
+                        } else if (inputValue == 3) {
+                            setLoadActive(2)  
+                        } else if (inputValue + 2 == postCollectionLength) {
+                            setLoadActive(2)
+                            loadActiveEnd = 2
+                        } else if (inputValue + 1 == postCollectionLength) {
+                            setLoadActive(3)
+                            loadActiveEnd = 3
+                        } else if (inputValue == postCollectionLength) {
+                            setLoadActive(4)
+                            loadActiveEnd = 4
+                        }
+                    }
+                }
+            })
+        })
+    }
 }
 
 const renderLoadList = () => {
+    loadActiveFlag = true
     loadList.innerHTML = ''
     if (postCollectionLength == 1) {
         leftArrow.innerHTML = ''
         rightArrow.innerHTML = ''
-    } else if (postCollectionLength > 1 && postCollectionLength < 5) {
+    } else if (postCollectionLength > 1 && postCollectionLength < 6) {
         for (let i = 0; i < postCollectionLength; i++) {
             createLoadItem(i+1)
         }
@@ -61,11 +108,22 @@ const renderLoadList = () => {
             for (let i = 0; i < 3; i++) createLoadItem(i+1)
             createLoadItem('...')
             createLoadItem(postCollectionLength)
-        } 
+            
+        } else if (currentPage >= postCollectionLength-2) {
+            loadActiveFlag = 'end'
+            createLoadItem(1)
+            createLoadItem('...')
+            for (let i = postCollectionLength-3; i < postCollectionLength; i++) {
+                createLoadItem(i+1)
+            }
+        }
         else {
+            loadActiveEnd = 1
+            loadActiveFlag = false
             createLoadItem(1)
             createLoadItem('...')
             createLoadItem(currentPage)
+            setLoadActive(2)
             createLoadItem('...')
             createLoadItem(postCollectionLength)
         }
@@ -74,41 +132,58 @@ const renderLoadList = () => {
 
 renderLoadList()
 
-
 const loadItems = document.getElementsByClassName('load_more-item')
+
+renderExtraLoad()
 
 loadItems[0].classList.add('active')
 leftArrow.classList.add('disabled')
 
-for (let i = 0; i < loadItems.length; i++) {
-    loadItems[i].addEventListener('click', () => {
-        // if (loadItems[i] == '...') {
-            
-        // } else {
-        currentPage = parseInt(loadItems[i].innerText)
-        setLoadActive(i)
-        renderPosts(currentPage)
-        buttonsControll()
-        // }
-    })
+const initLoad = () => {
+    renderLoadList()
+    renderExtraLoad()
+    setLoadItems()
+    renderPosts(currentPage)
+    buttonsControll()
 }
+
+const setLoadItems = () => {
+    for (let i = 0; i < loadItems.length; i++) {
+        loadItems[i].addEventListener('click', () => {
+            if (Number(loadItems[i].innerText)) {
+                loadActiveEnd = i
+                currentPage = Number(loadItems[i].innerText)
+                initLoad()
+                setLoadActive(i)
+            } 
+        })
+    }
+}
+
+setLoadItems()
 
 const switchToRight = () => {
     if (currentPage >= postCollectionLength) return
     currentPage += 1
-    // renderLoadList()
-    setLoadActive(currentPage-1)
-    renderPosts(currentPage)
-    buttonsControll()
+    initLoad()
+    if (loadActiveFlag == 'end') {
+        loadActiveEnd += 1
+        setLoadActive(loadActiveEnd)
+    } else if (loadActiveFlag) {
+        setLoadActive(currentPage-1)
+    }
 }
 
 const switchToLeft = () => {
     if (currentPage == 1) return
     currentPage -= 1
-    // renderLoadList()
-    setLoadActive(currentPage-1)
-    renderPosts(currentPage)
-    buttonsControll()
+    initLoad()
+    if (loadActiveFlag == 'end') {
+        loadActiveEnd -= 1
+        setLoadActive(loadActiveEnd)
+    } else if (loadActiveFlag) {
+        setLoadActive(currentPage-1)
+    }
 }
 
 rightArrow.addEventListener('click', switchToRight)
